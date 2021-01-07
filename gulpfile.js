87773bef9324ef3d8dbc3441071836623630f776
asadullo <asadullonazarov904@gmail.com>
2021-01-07 18:37:12 +0500

    add gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -45,3 +45,22 @@ gulp.task("watch", function () {
 });
 
 gulp.task("default", gulp.parallel("watch", "server", "styles"));
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// or...
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: "yourlocal.dev"
+    });
+});
